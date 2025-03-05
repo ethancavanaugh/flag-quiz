03d705311e5253ab5f525d16d6bda96be39abdd7
ethancavanaugh <ethancavanaugh@icloud.com>
2025-03-05 16:23:25 -0500

    Make a single guess and display result

--- src/main/java/com/ethancavanaugh/flagquiz/controllers/CountryController.java
+++ src/main/java/com/ethancavanaugh/flagquiz/controllers/CountryController.java
@@ -5,6 +5,8 @@ import com.ethancavanaugh.flagquiz.services.CountryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -18,16 +20,17 @@ public class CountryController {
         this.countryService = countryService;
     }
 
+    @GetMapping("/")
+    public String getRandomCountry(Model model){
+        Country country = countryService.getRandomCountry();
+        model.addAttribute("country", country);
+        return "single-country.html";
+    }
+
     @ResponseBody
     @GetMapping("/countries")
     public List<Country> getCountries(){
         return countryService.getAllCountries();
     }
 
-    @GetMapping("/countries/random")
-    public String getRandomCountry(Model model){
-        Country country = countryService.getRandomCountry();
-        model.addAttribute("country", country);
-        return "single-country.html";
-    }
 }
