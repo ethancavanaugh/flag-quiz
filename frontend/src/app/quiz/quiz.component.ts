a15da0bdf55f1c542a3ed46fd2a8919ef3986893
ethancavanaugh <ethancavanaugh@icloud.com>
2025-03-06 21:33:07 -0500

    Creates quiz component

---
+++ frontend/src/app/quiz/quiz.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quiz-content',
+  imports: [],
+  templateUrl: './quiz.component.html',
+  styleUrl: './quiz.component.scss'
+})
+export class QuizComponent {
+
+}
