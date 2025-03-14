8d3dc0a7c70a5c448ed331568103c13ccfd83ed3
ethancavanaugh <ethancavanaugh@icloud.com>
2025-03-14 18:59:59 -0400

    Clear input after correct guess

--- frontend/src/app/quiz/quiz.component.ts
+++ frontend/src/app/quiz/quiz.component.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CountryService } from '../country.service';
 import { Country } from '../country';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'quiz-content',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
@@ -13,27 +14,33 @@ export class QuizComponent {
   countryList!: Country[];
   curCountry!: Country;
   countryIdx: number = 0;
+  curGuess: string = "";
 
   constructor(private countryService: CountryService) {
     this.countryService.getAllCountries().subscribe(res => {
       this.countryList = res;
+      this.countryList = this.countryList.slice(0, 3); //TESTING ONLY
+
       this.curCountry = this.countryList.at(this.countryIdx)!;
     });
   }
 
-  handleTyping(guess: string) {
-    console.log(guess);
-    if (this.curCountry.validNames.includes(guess)){
+  handleTyping() {
+    console.log(this.curGuess);
+    if (this.curCountry.validNames.includes(this.curGuess)){
       console.log("Correct!");
       this.nextCountry();
     }
   }
 
   private nextCountry(){
-    if (this.countryIdx >= this.countryList.length){
+    this.curGuess = "";
+    
+    if (++this.countryIdx >= this.countryList.length){
       this.endGame();
+    } else {
+      this.curCountry = this.countryList.at(this.countryIdx)!
     }
-    this.curCountry = this.countryList.at(++this.countryIdx)!
   }
 
   private endGame(){
