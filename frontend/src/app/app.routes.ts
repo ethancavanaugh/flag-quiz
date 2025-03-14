0c50f2d1abf75d743de07a2c74fa053dffeed77c
ethancavanaugh <ethancavanaugh@icloud.com>
2025-03-14 19:24:02 -0400

    Adds routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultComponent } from './result/result.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '',
+    component: QuizComponent
+  },
+  {
+    path: 'result',
+    component: ResultComponent
+  }
+
+];
